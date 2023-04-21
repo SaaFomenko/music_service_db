2f408bf75ad660e30a869b201e9b75441a0f71be
SaaFomenko <s.fomenko@list.ru>
2023-04-21 12:06:10 +0300

    Save complict issue 1.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # music_service_db
 Demo sql code and ER-diagram for music service.
+
+Condition you see this [link](https://github.com/netology-code/sqlcpp-homeworks/tree/main/02)
+
+## ER-diagram
+![ER-diagram for music service](./music_service_db.png)
+
+## SQL code
+<./create_tb.sql>
---
+++ create_tb.sql
@@ -0,0 +1,43 @@
+CREATE TABLE IF NOT EXISTS genre (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(32) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS musician (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(64) UNIQUE NOT NULL,
+	genre_id INTEGER NOT NULL REFERENCES genre(id)
+);
+
+ALTER TABLE genre ADD COLUMN 
+	musician_id INTEGER NOT NULL REFERENCES musician(id);
+
+CREATE TABLE IF NOT EXISTS album (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(80) UNIQUE NOT NULL,
+	publication DATE NOT NULL,
+	musician_id INTEGER NOT NULL REFERENCES musician(id)
+);
+
+ALTER TABLE musician ADD COLUMN
+	album_id INTEGER REFERENCES album(id);
+
+CREATE TABLE IF NOT EXISTS track (
+	id SERIAL PRIMARY KEY REFERENCES album(id),
+	name VARCHAR(128) UNIQUE NOT NULL,
+	duration TIME NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS collection (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(128) UNIQUE NOT NULL,
+	publication DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS collection_track (
+	collection_id INTEGER REFERENCES collection(id),
+	track_id INTEGER REFERENCES track(id)
+);
+
+ALTER TABLE collection_track ADD 
+	CONSTRAINT pk PRIMARY KEY (collection_id, track_id);
